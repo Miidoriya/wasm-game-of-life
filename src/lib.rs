4b14bcfe8ecae45af6fda0b3d68a3db515fd4115
Miidoriya <99621562+Miidoriya@users.noreply.github.com>
2023-01-31 20:12:20 +0000

    [implementing-life]
    Working through https://rustwasm.github.io/docs/book/game-of-life/implementing.html

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,39 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+#[repr(u8)] // represents each variant as a single byte
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1, // Use of 1 means we can count live neighbors by summing
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(format!("Hello, {}!", name).as_str());
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbour_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width -1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
 }
